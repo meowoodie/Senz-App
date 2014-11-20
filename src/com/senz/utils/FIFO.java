5f12f5a5c4568cf4efc3636a24b0f83a3ed05afe
meowoodie <woodielove@163.com>
2014-11-20 21:17:06 +0800

    create file - FIFO

---
+++ src/com/senz/utils/FIFO.java
@@ -0,0 +1,24 @@
+package com.senz.utils;
+
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * Created by woodie on 14/11/20.
+ */
+interface FIFO<T> extends List<T>, Deque<T>, Cloneable, java.io.Serializable {
+
+    // Add a new element to the last of list,
+    // If there is already full, then poll an element out.
+    T addLastSafe(T addLast);
+
+    // Poll the head of list, if there is not exist, then return null
+    T pollSafe();
+
+    // Get the max size of list
+    int getMaxSize();
+
+    // Set the max size of list, if the new size is smaller then the number of existing elements, then poll then out
+    List<T> setMaxSize(int maxSize);
+
+}
--- src/com/senz/utils/Queue.java
+++ src/com/senz/utils/Queue.java
@@ -12,23 +12,6 @@ import java.util.List;
  * @Description:
  ***********************************************************************************************************************/
 
-interface FIFO<T> extends List<T>, Deque<T>, Cloneable, java.io.Serializable {
-
-    // Add a new element to the last of list,
-    // If there is already full, then poll an element out.
-    T addLastSafe(T addLast);
-
-    // Poll the head of list, if there is not exist, then return null
-    T pollSafe();
-
-    // Get the max size of list
-    int getMaxSize();
-
-    // Set the max size of list, if the new size is smaller then the number of existing elements, then poll then out
-    List<T> setMaxSize(int maxSize);
-
-}
-
 class Queue<T> extends LinkedList<T> implements FIFO<T> {
 
     private int maxSize = Integer.MAX_VALUE;
