4716bc1684d41f60d5b579aaa381f463ae7466a0
meowoodie <woodielove@163.com>
2014-11-20 21:13:02 +0800

    revise the queue

--- src/com/senz/utils/Queue.java
+++ src/com/senz/utils/Queue.java
@@ -1,6 +1,9 @@
 package com.senz.utils;
 
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 
 /***********************************************************************************************************************
  * @ClassName:   Queue
@@ -9,18 +12,73 @@ import java.util.LinkedList;
  * @Description:
  ***********************************************************************************************************************/
 
-public class Queue<E> extends LinkedList<E> {
-    private int limit;
+interface FIFO<T> extends List<T>, Deque<T>, Cloneable, java.io.Serializable {
 
-    public Queue(int limit) {
-        this.limit = limit;
+    // Add a new element to the last of list,
+    // If there is already full, then poll an element out.
+    T addLastSafe(T addLast);
+
+    // Poll the head of list, if there is not exist, then return null
+    T pollSafe();
+
+    // Get the max size of list
+    int getMaxSize();
+
+    // Set the max size of list, if the new size is smaller then the number of existing elements, then poll then out
+    List<T> setMaxSize(int maxSize);
+
+}
+
+class Queue<T> extends LinkedList<T> implements FIFO<T> {
+
+    private int maxSize = Integer.MAX_VALUE;
+    private final Object synObj = new Object();
+
+    public Queue() {
+        super();
+    }
+
+    public Queue(int maxSize) {
+        super();
+        this.maxSize = maxSize;
     }
 
     @Override
-    public boolean add(E o) {
-        super.add(o);
-        while (size() > limit) { super.remove(); }
-        return true;
+    public T addLastSafe(T addLast) {
+        synchronized (synObj) {
+            T head = null;
+            while (size() >= maxSize) {
+                head = poll();
+            }
+            addLast(addLast);
+            return head;
+        }
     }
 
+    @Override
+    public T pollSafe() {
+        synchronized (synObj) {
+            return poll();
+        }
+    }
+
+    @Override
+    public List<T> setMaxSize(int maxSize) {
+        List<T> list = null;
+        if (maxSize < this.maxSize) {
+            list = new ArrayList<T>();
+            synchronized (synObj) {
+                while (size() > maxSize) {
+                    list.add(poll());
+                }
+            }
+        }
+        this.maxSize = maxSize;
+        return list;
+    }
+
+    @Override
+    public int getMaxSize() {
+        return this.maxSize;
+    }
 }
